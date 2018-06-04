c5ae2e081f47982ff25d967dea0dfdca136c7279
Unknown <andrivgar@gmail.com>
2018-06-04 19:30:42 +0200

    final project 2

---
+++ Final Project/js/database.js
@@ -0,0 +1,12 @@
+
+
+module.exports = {
+    'connection': {
+        'host': 'sql7.freemysqlhosting.net',
+        'user': 'sql7241207',
+        'password': 'dsuKK8PAt3',
+        'database': 'db'
+    },
+    'database': 'db',
+
+};
